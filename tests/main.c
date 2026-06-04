57154a659bd4613267f9da97e37d6362b78955d2
Alex Saveliev <alexvsaveliev@gmail.com>
2026-06-04 15:39:10 +0900

    inital test file, just tests random for now
    run with (after compiling random)
    gcc -I include/ tests/main.c out/random -o out/test && ./out/test

---
+++ tests/main.c
@@ -0,0 +1,32 @@
+
+// root test file
+// will call other tests
+
+#include <stdio.h>
+#include <stdint.h>
+#include "random.h"
+
+void test_random() {
+    // call into random
+    // now we're at the same problem 
+    // in that idk how to incldue stuff
+
+    uint64_t seed = 603;
+
+    uint64_t* rand = rand_init(seed);
+    printf("%llu\n", *rand);
+    rand_next(rand);
+    printf("%llu\n", *rand);
+    
+    
+    printf("testing random\n");
+
+    rand_free(rand);
+}
+
+int main(int argc, char* argv[]){
+    // Random testing section
+    test_random();
+
+    return 0;
+}
