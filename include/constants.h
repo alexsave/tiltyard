85143faa6a4360802a585f0fadd5918879e9d861
Alex Saveliev <alexvsaveliev@gmail.com>
2026-06-15 12:30:45 +0900

    moving shared constants to header file

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "types.h"
+
+static const u64 SERVER_TYPE = 0;
+static const u64 CLIENT_IN_TYPE = 1;
+static const u64 CLIENT_OUT_TYPE = 2;
+static const u64 SLOW_REPEAT_TYPE = 3;
+
+#endif
+
